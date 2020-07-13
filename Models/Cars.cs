129294db2fc2ffabc1e1344769dcd64eb39ff407
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-13 14:33:25 -0700

    Add auto-implemented properties with get and set shorthand.
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>

--- Models/Cars.cs
+++ Models/Cars.cs
@@ -7,11 +7,20 @@ namespace Dealership.Models {
     private int _price;
     private int _miles;
 
+    public string MakeModel{ get; set;}
+    public int Price { get; set;}
+    public int Miles { get; set;}
+
     public Car(string makeModel, int price, int miles)
     {
-      _makeModel = makeModel;
-      _price = price;
-      _miles = miles;
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public static string MakeSound(string sound)
+    {
+      return "Our cars sound like " + sound;
     }
 
     public string GetMakeModel()
